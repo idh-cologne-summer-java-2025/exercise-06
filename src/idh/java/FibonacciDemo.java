ecf398bb3a1556460b5ad5bcb153f37c7e5cda0b
moritz <moritzleo07@gmail.com>
2025-05-28 14:01:24 +0200

    All extra solutions

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -4,8 +4,41 @@ public class FibonacciDemo {
 
 	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
 	public static int fibonacci(int n) {
-		//TODO Implement!
+		if(n == 0) {
+			return 0; 
+		} else if(n == 1) {
+			return 1; 
+		} else if(n > 1) {
+			return fibonacci(n - 1) + fibonacci(n - 2); 
+		}
 		return -1; 
+	}
+
+	public static int fibonacciIterative(int n) {
+		if (n == 0) return 0;
+		if (n == 1) return 1;
+
+		int vorletzter = 0,letzter = 1, nächster = 0;
+		for (int i = 2; i <= n; i++) {
+			nächster = letzter + vorletzter;
+			vorletzter = letzter;
+			letzter = nächster;
+		}
+		return nächster;
+	}
+
+	public static int fibonacciMemoization(int n) {
+		if (n == 0)	return 0;
+		if (n==1)	return 1;
+
+		int[] memo = new int[n + 1];
+		memo[0] = 0;
+		memo[1] = 1;
+
+		for(int i = 2; i <= n; i++) {
+			memo[i] = memo[i - 1] + memo[i - 2];
+		}
+		return memo[n];
 	}
 
 	
@@ -14,6 +47,9 @@ public class FibonacciDemo {
 		System.out.println("Die ersten " + n + " Fibonacci-Zahlen:");
 		for (int i = 0; i < n; i++) {
 			System.out.print(fibonacci(i) + " ");
+			System.out.print(fibonacciIterative(i) + " ");
+			System.out.print(fibonacciMemoization(i) + " ");
+
 		}
 	}
 
