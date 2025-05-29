a430718894e024dcc7300e01ad5e6197801aaa12
gabriela <gabriela@gabrielas-air>
2025-05-29 11:02:53 +0200

    Abgabe excercise 6

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -5,7 +5,14 @@ public class FibonacciDemo {
 	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
 	public static int fibonacci(int n) {
 		//TODO Implement!
-		return -1; 
+		if (n==0) {
+		return 0; //Abbruchfall 1
+		} else if (n==1) {
+			return 1; // Abbruchfall 2
+			
+		} else {
+			return fibonacci(n-1) + fibonacci (n-2); // Rekursiver Aufruf
+		}
 	}
 
 	
