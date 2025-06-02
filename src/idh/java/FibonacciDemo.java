b598e29689f76155ee9170c696cfadd87be14190
Lydianoki <elchkeks@schreiber-ehle.de>
2025-06-02 23:10:08 +0200

    Ex_06 Lars Ehle

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -2,15 +2,21 @@ package idh.java;
 
 public class FibonacciDemo {
 
-	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
+	
 	public static int fibonacci(int n) {
-		//TODO Implement!
-		return -1; 
+		 if(n == 0)
+		        return 0;
+		 
+		    else if(n == 1)
+		      return 1;
+		 
+		   else
+		      return fibonacci(n - 1) + fibonacci(n - 2);
 	}
 
 	
 	public static void main(String[] args) {
-		int n = 10; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
+		int n = 20; 
 		System.out.println("Die ersten " + n + " Fibonacci-Zahlen:");
 		for (int i = 0; i < n; i++) {
 			System.out.print(fibonacci(i) + " ");
