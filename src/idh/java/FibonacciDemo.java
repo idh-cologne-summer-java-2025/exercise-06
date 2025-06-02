a65b62e345e429385529ecbfdc01723403efb01f
Yuima <Yuima@DwieDosenlaptop>
2025-06-02 20:17:45 +0200

    Das ist eine vereinfachte den Code in der MainFunktion auch leicht abgeänderte Version, wie man die Fibonacci-Zahlen bekommen kann, ohne eine komische Schleife in der Mainfunktion zu haben.

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -2,19 +2,32 @@ package idh.java;
 
 public class FibonacciDemo {
 
+	static int f = 0;
+	static int iFormer = 0;
+	static int iNew = 1;
+	
 	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
 	public static int fibonacci(int n) {
 		//TODO Implement!
-		return -1; 
+		if (n>0) {
+			fibonacci(n-1);
+			}
+		f = iFormer + iNew;
+		System.out.println(f);
+		iFormer = iNew;
+		iNew = f;
+		return f;
+		
 	}
 
-	
+
 	public static void main(String[] args) {
 		int n = 10; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
 		System.out.println("Die ersten " + n + " Fibonacci-Zahlen:");
-		for (int i = 0; i < n; i++) {
-			System.out.print(fibonacci(i) + " ");
-		}
+		fibonacci(n);
+		//for (int i = 0; i < n; i++) {
+		//	System.out.print(fibonacci(i) + " ");
+		//}
 	}
 
 }
