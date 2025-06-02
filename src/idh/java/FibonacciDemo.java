5ced9e39a1b13a01fb10aa3a2c6dcf8c9e4dd577
Hala Alkilani <hala.alkilani002@gmail.com>
2025-06-02 23:15:33 +0200

    exercise 06

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -4,15 +4,23 @@ public class FibonacciDemo {
 
 	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
 	public static int fibonacci(int n) {
-		//TODO Implement!
-		return -1; 
-	}
+		if (n == 0) { //Basisfall 
+			return 0;
+		} else if (n == 1) { //Basisfall
+			return 1;
+		//Beide Basisfälle sind Abbruchbedingungen	
+		}
+			//
+		return fibonacci(n-1) + fibonacci(n-2); //Fibonacci-Berechnung
+		}
+	
+
 
 	
 	public static void main(String[] args) {
 		int n = 10; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
 		System.out.println("Die ersten " + n + " Fibonacci-Zahlen:");
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) { //zählt von 0 bis 9 (also 10-mal)
 			System.out.print(fibonacci(i) + " ");
 		}
 	}
