9bc7e6836f5a3c699d5d7eacb4bbf4f295012647
faran230 <130466482+faran230@users.noreply.github.com>
2025-06-03 21:33:10 +0200

    Übung 6

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -2,24 +2,22 @@ package idh.java;
 
 public class FibonacciDemo {
 
-	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
-	public static int fibonacci(int n) {
-		if (n == 0) {
-			return 0;
-		} else if (n == 1) {
-			return 1;
-		} else {
-			return fibonacci(n - 1) + fibonacci(n - 2);
-		}
-	}
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 
-	
-	public static void main(String[] args) {
-		int n = 10; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
-		System.out.println("Die ersten " + n + " Fibonacci-Zahlen:");
-		for (int i = 0; i < n; i++) {
-			System.out.print(fibonacci(i) + " ");
-		}
-	}
+    public static void main(String[] args) {
+        int n = 10;
 
+        System.out.println("Fibonacci-Zahlen bis " + n + ":");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
 }
