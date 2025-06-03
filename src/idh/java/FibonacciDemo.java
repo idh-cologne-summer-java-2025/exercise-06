60293e4d08ab5b94f4eeec840c124dddccbdad57
edylyn <edylyn@localhost>
2025-06-03 21:36:05 +0200

    Exercise-06

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -4,13 +4,16 @@ public class FibonacciDemo {
 
 	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
 	public static int fibonacci(int n) {
-		if (n == 0) {
-			return 0;
-		} else if (n == 1) {
-			return 1;
-		} else {
-			return fibonacci(n - 1) + fibonacci(n - 2);
+		if (n == 0) return 0;
+		if (n == 1) return 1;
+
+		int a = 0, b = 1;
+		for (int i = 2; i <= n; i++) {
+			int temp = a + b;
+			a = b;
+			b = temp;
 		}
+		return b;
 	}
 
 	
@@ -23,3 +26,5 @@ public class FibonacciDemo {
 	}
 
 }
+
+
