887d8ee57303e308f9c436135c3a0b0154eb0447
efpis <efpis@pc.fritz.box>
2025-06-03 15:20:59 +0200

    aufgabe6

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -4,8 +4,13 @@ public class FibonacciDemo {
 
 	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
 	public static int fibonacci(int n) {
-		//TODO Implement!
-		return -1; 
+		if (n == 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
 	}
 
 	
