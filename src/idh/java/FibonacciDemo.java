9491ccca0845eca39b793580efb40b6cd7536894
java2-er <eramgit@gmail.com>
2025-06-03 07:42:22 +0200

    init

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -4,11 +4,15 @@ public class FibonacciDemo {
 
 	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
 	public static int fibonacci(int n) {
-		//TODO Implement!
-		return -1; 
+		if (n <= 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
 	}
 
-	
 	public static void main(String[] args) {
 		int n = 10; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
 		System.out.println("Die ersten " + n + " Fibonacci-Zahlen:");
@@ -16,5 +20,4 @@ public class FibonacciDemo {
 			System.out.print(fibonacci(i) + " ");
 		}
 	}
-
 }
