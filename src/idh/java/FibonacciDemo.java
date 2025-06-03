ae3a6175340d0083cd9baf3b09ebcc725518c093
benediktbenz <benediktbenz@mac.fritz.box>
2025-06-04 01:34:57 +0200

    Ü6 effizienter

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -2,20 +2,20 @@ package idh.java;
 
 public class FibonacciDemo {
 
-    // Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
-    public static int fibonacci(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
+	public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
         }
+        return b;
+    
     }
 
     public static void main(String[] args) {
-        int n = 10; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
-        System.out.println("Die ersten " + n + " Fibonacci-Zahlen:");
+        int n = 40; 
         for (int i = 0; i < n; i++) {
             System.out.print(fibonacci(i) + " ");
         }
