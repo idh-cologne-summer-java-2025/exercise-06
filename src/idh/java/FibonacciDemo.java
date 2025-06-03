4fadd007860a25083ebc7aac1b3f45936566beee
Frankenstoned <frankenstein_1990@web.de>
2025-06-03 17:29:30 +0200

    huiiiiiiii

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -2,24 +2,29 @@ package idh.java;
 
 public class FibonacciDemo {
 
-	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
 	public static int fibonacci(int n) {
-		if (n == 0) {
-			return 0;
-		} else if (n == 1) {
-			return 1;
-		} else {
-			return fibonacci(n - 1) + fibonacci(n - 2);
-		}
-	}
+        if (n == 0) return 0;
+        if (n == 1) return 1;
 
+        int a = 0, b = 1, insult =0;
+
+        for (int i = 2; i <= n; i++) {
+            insult = a + b;
+            a = b;
+            b = insult;
+        }
+
+        return insult;
+			}
 	
 	public static void main(String[] args) {
-		int n = 10; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
-		System.out.println("Die ersten " + n + " Fibonacci-Zahlen:");
+		int n = 47; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
+		System.out.println("Die ersten " + n + " Fibonacci-Zahlen schimpfen sich wie folgt und die letzte ist ein:");
 		for (int i = 0; i < n; i++) {
 			System.out.print(fibonacci(i) + " ");
 		}
 	}
 
 }
+
+
