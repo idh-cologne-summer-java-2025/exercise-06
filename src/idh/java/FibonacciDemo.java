1cc4bb27cfec14e24b204685ef2c11eb830cdf69
mbensche <Marc@LaptopMarc>
2025-06-04 12:01:29 +0200

    initial commit

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -12,6 +12,35 @@ public class FibonacciDemo {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
+//    bad idea #2
+    public static int fibonacci2(int n, int ceil) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        else if(n==ceil) {
+        	return fibonacci(ceil);
+        }
+        else {
+            int res = fibonacci(n - 1) + fibonacci(n - 2);
+            System.out.println(res);
+            fibonacci2(n++, ceil);
+            return res;
+        }
+    }
+    
+    public static int fibonacci3(int n) {
+    	 if (n == 0) {
+             return 0;
+         } else if (n == 1) {
+             return 1;
+         } else {
+             return fibonacci(n - 1) + fibonacci(n - 2);
+         }
+     }	
+    
+    
 
     public static void main(String[] args) {
         int n = 10; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
@@ -19,5 +48,7 @@ public class FibonacciDemo {
         for (int i = 0; i < n; i++) {
             System.out.print(fibonacci(i) + " ");
         }
+//    	System.out.println(fibonacci2(0,100));
+    
     }
 }
