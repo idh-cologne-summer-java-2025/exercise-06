131e4e17b23a85480b47c430291424625f7a0ef8
alinaleverenz <alinaleverenz@localhost>
2025-06-25 14:22:36 +0200

    Aufgabe 6

--- src/idh/java/FibonacciDemo.java
+++ src/idh/java/FibonacciDemo.java
@@ -4,13 +4,19 @@ public class FibonacciDemo {
 
 	// Rekursive Methode zur Berechnung der n-ten Fibonacci-Zahl
 	public static int fibonacci(int n) {
-		//TODO Implement!
-		return -1; 
+		if (n == 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+		 
 	}
 
 	
 	public static void main(String[] args) {
-		int n = 10; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
+		int n = 20; // Anzahl der Fibonacci-Zahlen, die berechnet werden sollen
 		System.out.println("Die ersten " + n + " Fibonacci-Zahlen:");
 		for (int i = 0; i < n; i++) {
 			System.out.print(fibonacci(i) + " ");
